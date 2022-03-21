88aa288262db68c1ad4bbee733f5e37238706359
Jora <tjorav@gmail.com>
2022-03-21 18:45:33 +0300

    [fix] Popular image fix

--- files/main.js
+++ files/main.js
@@ -1020,7 +1020,7 @@ function compare() {
 		autoHeight: true,
 		autoHeightClass: 'owl-height',
 		autoplay: false,
-		autoplayHoverPause: true,
+		autoplayHoverPause: false,
 		smartSpeed: 500,
 		mouseDrag: false,
 		touchDrag: false,
